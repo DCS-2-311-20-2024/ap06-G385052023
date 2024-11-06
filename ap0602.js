fbec7e62d57b2b01aaaba65b48c82ce2e580f72d
238505@st.takushoku-u.ac.jp <238505@st.takushoku-u.ac.jp>
2024-11-06 17:19:27 +0900

    映画館
    映画館を作った

--- ap0602.js
+++ ap0602.js
@@ -7,59 +7,76 @@
 
 // ライブラリをモジュールとして読み込む
 import * as THREE from "three";
-import {CSS3DRenderer, CSS3DObject} from "three/addons"
+import { CSS3DRenderer, CSS3DObject } from "three/addons"
 
 // ３Ｄページ作成関数の定義
 function init() {
   // 制御変数の定義
-  const param = {w:0.6, h:0.1, d:0.6,
-     nRow:7, nCol:11, gapX:0.1, gapY:0.3, gapZ:0.4};
+  const param = {
+    w: 0.6, h: 0.1, d: 0.6,
+    nRow: 7, nCol: 11, gapX: 0.1, gapY: 0.3, gapZ: 0.4
+  };
 
   // シーン作成
   const scene = new THREE.Scene();
 
   // 第1のレンダラ
-  const nameHeight = document.getElementById("name").clientHeight;
+  const nameHeight = document.getElementById("output1").clientHeight;
   const renderer = new THREE.WebGLRenderer();
   {
     renderer.setClearColor(0x204060);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(
-      window.innerWidth,
-      window.innerHeight - nameHeight);
+      0.7 * window.innerWidth,
+      0.5 * window.innerWidth);
     renderer.domElement.style.position = "absolute";
     renderer.domElement.style.zIndex = 1;
     renderer.domElement.style.top = nameHeight;
   }
   // 第2のレンダラ
-
+  const renderer2 = new THREE.WebGLRenderer();
+  {
+    renderer2.setClearColor(0x207070);
+    renderer2.setPixelRatio(window.devicePixelRatio);
+    renderer2.setSize(
+      0.3 * window.innerWidth,
+      0.6 * window.innerWidth);
+    renderer2.domElement.style.position = "absolute";
+    renderer2.domElement.style.zIndex = 1;
+    renderer2.domElement.style.top = nameHeight;
+  }
   // CSS3Dレンダラ
   const cssRenderer = new CSS3DRenderer();
   {
     cssRenderer.setSize(
-      window.innerWidth,
-      window.innerHeight - nameHeight);
+      0.7 * window.innerWidth,
+      0.5 * window.innerWidth);
     cssRenderer.domElement.style.position = "absolute";
     cssRenderer.domElement.style.zIndex = 0;
     cssRenderer.domElement.style.top = nameHeight;
   }
 
   // カメラの作成
-   const camera1 = new THREE.PerspectiveCamera(
-    50, window.innerWidth/(window.innerHeight-nameHeight), 0.1, 1000);
+  const camera1 = new THREE.PerspectiveCamera(
+    50, 7 / 5, 0.1, 1000);
   {
-    camera1.position.set(0,2,15);
-    camera1.lookAt(0,0,0);
-    // camera1.position.set(-1,0,3);
-    // camera1.lookAt(0,-1.5,5);
+    camera1.position.set(0, 2, 15);
+    camera1.lookAt(0, 0, 0);
+    // camera1.position.set(-1, 0, 0);
+    // camera1.lookAt(0, -1.5, 5);
   }
   // 第2のカメラ
- 
+  const camera2 = new THREE.PerspectiveCamera(
+    50, 1 / 2, 0.1, 1000);
+  {
+    camera2.position.set(0, 18, 6);
+    camera2.lookAt(0, 0, 6);
+  }
 
   // 光源の設定
   { // 環境ライト
     const light = new THREE.AmbientLight();
-    light.intensity=0.4;
+    light.intensity = 0.4;
     scene.add(light);
   }
   { // ポイントライト
@@ -71,30 +88,98 @@ function init() {
   // 3D物体
   // スクリーン
   const screen = new THREE.Mesh(
-
+    new THREE.PlaneGeometry(8.0, 4.5),
+    new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      opacity: 0.0,
+      blending: THREE.NoBlending,
+      side: THREE.DoubleSide
+    })
   )
-  screen.position.set(0,0,0);
-  // scene.add(screen);
-  
+  screen.position.set(0, 0, 0);
+  scene.add(screen);
+
   // 椅子
   const chair = new THREE.Group();
   {
-
+    const chairMaterial = new THREE.MeshLambertMaterial({ color: 0x802000 });
+    const ch1 = new THREE.Mesh(
+      new THREE.BoxGeometry(param.w, param.h, param.d),
+      chairMaterial);
+    ch1.position.set(0, 0.3, 0);
+    chair.add(ch1);
+    const ch2 = new THREE.Mesh(
+      new THREE.BoxGeometry(param.w, param.d, param.h),
+      chairMaterial);
+    ch2.position.set(0, 0.3 + (param.h + param.d) / 2, (param.h + param.d) / 2);
+    chair.add(ch2);
   }
   // 座席の生成
+  // chair.position.set(0,-2,5);
+  // scene.add(chair);
+  const seats = new THREE.Group();
+  for (let r = 0; r < param.nRow; r += 1) {
+    for (let c = 0; c < param.nCol; c += 1) {
+      const seat = chair.clone();
+      seat.position.set(
+        (param.w + param.gapX) * (c - (param.nCol - 1) / 2),
+        (param.gapY) * r - 2,
+        (param.d + param.gapZ) * (r + 5)
+      )
+      seats.add(seat);
+    }
+  }
+  scene.add(seats);
 
   // ダミーを作る関数
   function makeDummy(color) {
+    const dummy = new THREE.Group();
+    {
+      const dhead = new THREE.Mesh(
+        new THREE.SphereGeometry(0.15, 12, 12),
+        new THREE.MeshLambertMaterial({ color: color })
+      )
+      dummy.add(dhead);
+      const dbody = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.2, 0.2, 0.3, 12),
+        new THREE.MeshLambertMaterial({ color: color })
+      )
 
+      dbody.position.set(0, -0.3, 0);
+      dummy.add(dbody);
+    }
+    return dummy;
+  }
+  // 他の観客
+  for (let r = 0; r < param.nRow; r += 1) {
+    for (let c = 0; c < param.nCol; c += 1) {
+      if (Math.random() < 0.4) {
+        const dummy = makeDummy("red");
+        dummy.position.set(
+          (param.w + param.gapX) * (c - (param.nCol - 1) / 2),
+          (param.gapY) * r - 1.2,
+          (param.d + param.gapZ) * (r + 5)
+        )
+        scene.add(dummy);
+      }
+    }
   }
 
-  // 他の観客
-
   // アバターの生成
+  const avatar = makeDummy("white");
+  setAvatar(
+    new THREE.Vector3(0,
+      3 * param.gapY - 2,
+      3 * (param.d + param.gapZ) + 5)
+  )
+  scene.add(avatar);
 
   // アバターの移動
-  function setAvatar(position){
-
+  function setAvatar(position) {
+    avatar.position.copy(position);
+    avatar.position.y += 0.85;
+    camera1.position.copy(avatar.position);
+    camera1.lookAt(0, 0, 0);
   }
 
   // CSS3D表示のための設定
@@ -104,42 +189,56 @@ function init() {
   iframe.style.height = "360px";
   iframe.style.border = "0px";
   iframe.src = "https://www.youtube.com/embed?version=3"
-      +"&mute=1&autoplay=1&controls=0"
-      +"&loop=1&playlist=w23RIKTYF28,-9pMuSNlN6A";
-  
+    + "&mute=1&autoplay=1&controls=0"
+    + "&loop=1&playlist=w23RIKTYF28,-9pMuSNlN6A";
+
   // CSSオブジェクトの生成
   const cssObject = new CSS3DObject(iframe);
-  cssObject.scale.x *= 8/640;
-  cssObject.scale.y *= 8/640;
+  cssObject.scale.x *= 8 / 640;
+  cssObject.scale.y *= 8 / 640;
   scene.add(cssObject);
 
   // レンダラーの配置
   document.getElementById("output1").appendChild(cssRenderer.domElement);
-  // document.getElementById("output1").appendChild(renderer.domElement);
+  document.getElementById("output1").appendChild(renderer.domElement);
+  document.getElementById("output2").appendChild(renderer2.domElement);
 
   // シート選択のための設定
   const raycaster = new THREE.Raycaster();
   const mouse = new THREE.Vector2();
   function onMouseDown(event) {
     // マウスの位置を±1の範囲に変換
-  
-    // 光線を発射
-    
+    mouse.x = (event.clientX / window.innerWidth - 0.7) * 20 / 3 - 1;
+    if (mouse.x < -1) {
+      mouse.x = -1;
+    }
+    mouse.y = -(event.clientY / (window.innerWidth * 0.6)) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera2);
+
     // 全ての座席について
- 
+    seats.children.forEach((seat) => {
+      const intersects = raycaster.intersectObject(seat, true);
+      if (intersects.length > 0) {
+        setAvatar(intersects[0].object.parent.position);
+      }
+    });
   }
   window.addEventListener("mousedown", onMouseDown, false);
 
   // Windowサイズの変更処理
-  window.addEventListener("resize", ()=>{
-    // cssRenderer.setSize( window.innerWidth, window.innerHeight-nameHeight );
-    // renderer.setSize( window.innerWidth, window.innerHeight-nameHeight );
+  window.addEventListener("resize", () => {
+    // camera1.updateProjectionMatrix();
+    // camera2.updateProjectionMatrix();
+    cssRenderer.setSize(0.7 * window.innerWidth, 0.5 * window.innerWidth);
+    renderer.setSize(0.7 * window.innerWidth, 0.5 * window.innerWidth);
+    renderer2.setSize(0.3 * window.innerWidth, 0.6 * window.innerWidth);
   }, false);
 
   // 描画処理
   function update(time) {
     cssRenderer.render(scene, camera1);
-    // renderer.render(scene, camera1);
+    renderer.render(scene, camera1);
+    renderer2.render(scene, camera2);
     requestAnimationFrame(update);
   }
 
